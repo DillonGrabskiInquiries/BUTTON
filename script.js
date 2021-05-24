e4d76c712447e009f02718c72d3a47a460a7572d
DillonGrabskiInquiries <sahasrahlainquiries@gmail.com>
2021-05-24 17:27:17 -0500

    yaaaaa

--- script.js
+++ script.js
@@ -6,3 +6,5 @@ function trackMouse(event) {
     console.log(event.clientX, event.clientY)
     body.style.backgroundColor = ('rgb(${event.clientX}, ${event.clientY}, ${event.clientX} - ${event.clientY}')
 }
+
+// ya
